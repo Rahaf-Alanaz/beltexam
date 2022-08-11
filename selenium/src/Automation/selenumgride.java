e8d618f5a91877bbb35969d3e53f0054cef9cc45
reham <reham@rehams-mbp>
2022-08-11 17:18:11 +0300

    mbnmn

---
+++ selenium/src/Automation/selenumgride.java
@@ -0,0 +1,22 @@
+package Automation;
+
+import javax.print.DocFlavor.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class selenumgride {
+
+	
+	selenumgride selenium = new DefaultSelenium(“localhost”, 4444, “*firefox”, “https://www.browserstack.com”);
+
+
+		DesiredCapabilities capability = DesiredCapabilities.firefox();
+
+	WebDriver driver = new FirefoxDriver();
+
+		WebDriver driver = new RemoteWebDriver(new URL("https://localhost:4444/wd/hub"), capability);
+
+}
